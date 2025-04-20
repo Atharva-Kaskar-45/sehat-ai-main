69c015bd42ea73eda699f6bded7274e4802a518c
Atharva Kaskar <124505200+Atharva-Kaskar-45@users.noreply.github.com>
2025-04-20 22:48:11 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes.sehat import sehat  # adjust import if file is elsewhere
+from routes.sehat import sehat
 from flask_cors import CORS
 import logging
 import os
@@ -10,6 +10,10 @@ CORS(app)  # enable CORS for frontend
 # Register blueprint
 app.register_blueprint(sehat, url_prefix='/api')
 
-if __name__ == '__main__':
+# This is required for Vercel
+if __name__ != '__main__':
+    application = app
+else:
     logging.basicConfig(level=logging.INFO)
     app.run(debug=True, port=5000)
+
