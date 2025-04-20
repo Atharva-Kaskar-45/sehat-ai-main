8d60a4105251501691a936544f355e822e31faf4
Atharva Kaskar <124505200+Atharva-Kaskar-45@users.noreply.github.com>
2025-04-20 23:17:42 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,12 @@
 from flask import Flask
-from routes.sehat import sehat
 from flask_cors import CORS
-import logging
-import os
 
 app = Flask(__name__)
-CORS(app)  # enable CORS for frontend
-
-# Register blueprint
-app.register_blueprint(sehat, url_prefix='/api')
-
-# This is required for Vercel
-if __name__ != '__main__':
-    application = app
-else:
-    logging.basicConfig(level=logging.INFO)
-    app.run(debug=True, port=5000)
-
+CORS(app, resources={
+    r"/api/*": {
+        "origins": [
+            "https://sehat-ai-main.vercel.app",
+            "http://localhost:3000"
+        ]
+    }
+})
